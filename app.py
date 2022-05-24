1c9860ac479ee7a846237db4c80adc01a843c171
Austin <austinlasseter@gmail.com>
2022-05-24 14:12:19 -0400

    updated the outputs

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ import plotly.graph_objects as go
 import plotly.express as px
 import pickle
 
+
+########### open the pickle file ######
+filename = open('analysis/tree_model.pkl', 'rb')
+tree_model = pickle.load(filename)
+filename.close()
+
 ########### define variables
 tabtitle='digits classifier'
 sourceurl = 'https://www.kaggle.com/burak3ergun/loan-data-set'
@@ -130,14 +136,15 @@ app.layout = html.Div(children=[
                 className='six columns'),
 
             html.Div([
-                html.H3('Classifier'),
+                html.H3('Predicted Digit'),
                 html.Br(),
+                html.H4('Random Forest Model:'),
+                html.H6(id='rf-prediction', children='...'),
+                html.H6(id='rf-probability', children='waiting for inputs'),
                 html.Br(),
-                html.Br(),
-                html.H6('Predicted Digit:'),
-                html.H1(id='prediction', children='...'),
-                html.H6('Probability:'),
-                html.H6(id='probability', children='waiting for inputs'),
+                html.H4('XGBoost Model:'),
+                html.H6(id='xgb-prediction', children='...'),
+                html.H6(id='xgb-probability', children='waiting for inputs'),
             ], className='three columns'),
         ], className="twelve columns"),
         html.Br(),
@@ -150,21 +157,21 @@ app.layout = html.Div(children=[
 ######### CALLBACK
 @app.callback(
                 Output('output-figure', 'figure'),
-                Output('prediction', 'children'),
-                Output('probability', 'children'),
+                Output('rf-prediction', 'children'),
+                Output('rf-probability', 'children'),
+                Output('xgb-prediction', 'children'),
+                Output('xgb-probability', 'children'),
               Input('canvas', 'json_data'))
 def update_data(string):
     if string:
         data = json.loads(string)
-        print(data['objects'][0]['path'])
+        print(data['objects'][0]['path']) # explore the contents of the shape file
         mask = parse_jsonstring(string, shape = (canvas_size, canvas_size))
-
-        img=(255 * mask).astype(np.uint8)
+        img=(255 * mask).astype(np.uint8) # transform the data
+        print(img) # explore the transformed data
         array_to_data_output = array_to_data_url(img)
-        # pickle the user input
-        filename = open('user-input-digit.pkl', 'wb')
-        pickle.dump(array_to_data_output, filename)
-        filename.close()
+        print(array_to_data_output)
+
         # display as heatmap
         fig = px.imshow(array_to_data_output, text_auto=True, color_continuous_scale='Blues')
         fig.layout.height = 600
@@ -172,11 +179,26 @@ def update_data(string):
         fig.update(layout_coloraxis_showscale=False)
         fig.update(layout_showlegend=False)
 
+        # convert the user input to the format expected by the model
+        some_digit_array = np.reshape(array_to_data_output.values, -1)
+        print('some_digit_array',[some_digit_array])
+        # make a prediction: Random Forest
+        rf_pred = tree_model.predict([some_digit_array])
+        rf_prob_array = tree_model.predict_proba([some_digit_array])
+        rf_prob = max(rf_prob_array[0])
+        rf_prob=round(rf_prob*100,2)
+
+        # make a prediction: XG Boost
+        xgb_pred = tree_model.predict([some_digit_array])
+        xgb_prob_array = tree_model.predict_proba([some_digit_array])
+        xgb_prob = max(xgb_prob_array[0])
+        xgb_prob=round(xgb_prob*100,2)
+
     else:
         raise PreventUpdate
 
 
-    return   fig,  '5', '95%'
+    return   fig,  f'Digit: {rf_pred[0]}', f'Probability: {rf_prob}%', f'Digit: {xgb_pred[0]}', f'Probability: {xgb_prob}%'
 
 
 if __name__ == '__main__':
